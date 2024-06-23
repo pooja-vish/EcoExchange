f5291574af623bc8c82e4130ef67d8d04e72c4a3
kausarfatema1 <78102128+kausarfatema1@users.noreply.github.com>
2024-06-23 03:37:08 -0400

    Product model creation

---
+++ product/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+from user_details.models import Member
+
+class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('PAPER', 'Paper'),
+        ('PLASTIC', 'Plastic'),
+        ('GLASS', 'Glass'),
+        ('METAL', 'Metal'),
+        ('ELECTRONICS', 'Electronics'),
+        ('ORGANIC', 'Organic'),
+        ('TEXTILES', 'Textiles'),
+        ('BATTERIES', 'Batteries'),
+        ('WOOD', 'Wood'),
+        ('RUBBER', 'Rubber'),
+        ('MIXED_MATERIALS', 'Mixed Materials'),
+    ]
+
+    product_id = models.AutoField(primary_key=True)
+    product_name = models.CharField(max_length=255)
+    product_description = models.TextField()
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    user = models.ForeignKey(Member, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.product_name
