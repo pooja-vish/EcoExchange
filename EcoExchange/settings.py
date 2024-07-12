250d4026d954d9b8552f91cc5fd1a3d1e61285c7
pooja-vish <vishwak@uwindsor.ca>
2024-07-12 12:33:25 -0400

    initial draft of product list page

--- EcoExchange/settings.py
+++ EcoExchange/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,11 +41,7 @@ INSTALLED_APPS = [
     'coins',
     'user_details.apps.UserDetailsConfig',
     'phonenumber_field',
-
     'product.apps.ProductConfig',
-
-
-
     'reviews',
     'order.apps.OrderConfig'
 
@@ -134,3 +130,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = 'product/static/product/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '')
+
+
--- product/views.py
+++ product/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.utils.text import Truncator
+
+from product.models import Product
+
 
 # Create your views here.
+
+def products_list(request):
+    product_list = Product.objects.all()
+    for product in product_list:
+        product.short_description = Truncator(product.product_description).chars(100)
+    return render(request,template_name='product/product_list.html',context={'product_list':product_list})
