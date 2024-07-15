be7401bc0a071ab9f0ad8d7af647b956b15c59e8
kausarfatema1 <78102128+kausarfatema1@users.noreply.github.com>
2024-07-15 12:35:04 -0400

    login backend created

--- user_details/views.py
+++ user_details/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def login_view(request):
+    error_message = None
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+
+            login(request, user)  # Ensure this line has `request` and `user`
+            return redirect('index')  # Replace 'home' with your desired URL name
+        else:
+            error_message = "Invalid email or password."
+    return render(request, 'user_details/login.html')
