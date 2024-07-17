3eb742320ed8b77ad1040686ab4c114a7f424ed4
rutvikonline2303 <46394526+rutvikonline2303@users.noreply.github.com>
2024-07-17 02:40:27 -0400

    Added Login and Registration Page

--- user_details/urls.py
+++ user_details/urls.py
@@ -3,7 +3,9 @@ from product import views
 from django.conf import settings
 from django.conf.urls.static import static
 from user_details import views
+from .forms import LoginForm
 
 urlpatterns = [
-   path('userdetails/',views.login_view,name='login_details'),
+   path('userdetails/', views.login_view, name='login_details'),
+   path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
 ]
--- user_details/views.py
+++ user_details/views.py
@@ -3,6 +3,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.views import View
+from user_details.forms import CustomerRegistrationForm
+from django.contrib import messages
+
 
 def login_view(request):
     error_message = None
@@ -21,3 +25,20 @@ def login_view(request):
                 error_message = "Invalid email or password."
 
     return render(request, 'user_details/login.html', {'form': form, 'error_message': error_message})
+
+
+class CustomerRegistrationView(View):
+    def get(self, request):
+        form = CustomerRegistrationForm()
+        return render(request, 'registration/customerregistration.html', {'form': form})
+
+    def post(self, request):
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()  # Save the form data to the database
+            messages.success(request, 'Your account has been created!')
+            return redirect('login_details')  # Redirect to login page after successful registration
+        # If form is not valid, render the registration page again with the form and errors
+        else:
+            messages.warning(request, 'Please correct the error below.')
+        return render(request, 'registration/customerregistration.html', {'form': form})
