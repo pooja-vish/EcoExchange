aa9d511380f02a403249df970075957bae95cded
Tanya Gautam <gautamt@uwindsor.ca>
2024-07-17 22:11:41 -0400

    added URL patterns and stuc HTML for template

--- product/product_url.py
+++ product/product_url.py
@@ -5,5 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     #path('', views.index, name='index'),
+    path('product/<int:pk>',views.product_detail,name='product_detail'),
     path('products_list/', views.products_list, name='product_list'),
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
