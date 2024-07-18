fcd0f0aba548ef923356d23760dce5412722f16b
Tanya Gautam <gautamt@uwindsor.ca>
2024-07-17 22:20:48 -0400

    added views

--- product/views.py
+++ product/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils.text import Truncator
 
 from product.models import Product
@@ -13,3 +13,6 @@ def products_list(request):
         product.short_description = Truncator(product.product_description).chars(100)
     return render(request,template_name='product/product_list.html',context={'product_list':product_list})
 
+def products_detail(request,product_id):
+    product = get_object_or_404(Product,id=product_id))
+    return render(request,template_name='product/product_detail.html',context={'product':product})
