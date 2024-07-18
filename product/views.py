3447554324ae8855542a48a389297d2aec1a9105
kausarfatema1 <78102128+kausarfatema1@users.noreply.github.com>
2024-07-17 23:34:42 -0400

    Some changes in the view

--- product/views.py
+++ product/views.py
@@ -26,9 +26,25 @@ def add_to_cart(request, product_id):
 
 @login_required
 def cart_detail(request):
-    cart_items = CartItem.objects.filter(user=request.user)
-    total_price = sum(item.get_total_price() for item in cart_items)
-    return render(request, 'product/cart_detail.html', {'cart_items': cart_items, 'total_price': total_price})
+    product = get_object_or_404(Product, id=product_id)
+    cart_item, created = CartItem.objects.get_or_create(user=request.user, product=product)
+
+    if product.quantity > 0:
+        cart_item.is_available = True
+        if created:
+            cart_item.quantity = 1  # Set initial quantity to 1
+        else:
+            if product.quantity > cart_item.quantity:
+                cart_item.quantity += 1  # Increment quantity if there's enough stock
+            else:
+                messages.error(request, 'Sorry, not enough quantity available for this product.')
+                return redirect('cart_detail')
+    else:
+        cart_item.is_available = False
+        messages.error(request, 'Sorry, this product is currently unavailable.')
+
+    cart_item.save()
+    return redirect('cart_detail')
 
 def homepage(request):
     product_list = Product.objects.all()
