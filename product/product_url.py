9ad78dbb2e3d00a426662537bf43f9eea32e8100
Tanya Gautam <gautamt@uwindsor.ca>
2024-07-19 02:25:14 -0400

    base.html minor change,adding aboutUs

--- product/product_url.py
+++ product/product_url.py
@@ -7,5 +7,6 @@ urlpatterns = [
     #path('', views.index, name='index'),
     path('product/<int:pk>',views.product_detail,name='product_detail'),
     path('products_list/', views.products_list, name='product_list'),
+    path('aboutus/',views.aboutus,name='aboutus'),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- product/views.py
+++ product/views.py
@@ -6,7 +6,6 @@ from product.models import Product
 
 # Create your views here.
 
-
 def products_list(request):
     product_list = Product.objects.all()
     for product in product_list:
@@ -16,3 +15,6 @@ def products_list(request):
 def product_detail(request,pk):
     product = get_object_or_404(Product,pk=pk)
     return render(request,template_name='product/product_detail.html',context={'product':product})
+
+def aboutus (request):
+    return render(request,'product/aboutus.html')
