a99c3371785f8984599a2332e44bc853ae29d41b
Gurekam Singh <bedi61@uwindsor.ca>
2024-07-25 14:52:08 -0400

    orderstatus

--- order/urls.py
+++ order/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('order/', views.user_orders, name='user_orders'),
     path('onsale/', views.seller_products, name='ordered_products'),
+    path('update_order_status/<int:order_id>/', views.update_order_status, name='update_order_status'),
+
 
 ]
--- order/views.py
+++ order/views.py
@@ -1,8 +1,10 @@
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Order, OrderItem
 from product.models import Product
 from user_details.models import Member
+from django.utils import timezone
+
 
 def user_orders(request):
     try:
@@ -19,9 +21,20 @@ def seller_products(request):
         member = Member.objects.get(username=request.user.username)
     except Member.DoesNotExist:
         member = None
-    products = Product.objects.filter(user = member)
+
+    products = Product.objects.filter(user=member)
     order_items1 = OrderItem.objects.filter(product_id__in=products)
     orders1 = Order.objects.filter(order_id__in=[item.order_id.order_id for item in order_items1])
     users = {order.user_id for order in orders1}
-    context = {'products': products, 'order_items1': order_items1, 'orders1': orders1, 'users': users, }
+    context = {'products': products, 'order_items1': order_items1, 'orders1': orders1, 'users': users}
     return render(request, 'order/seller_products.html', context)
+
+
+def update_order_status(request, order_id):
+    print("hello")
+    if request.method == "POST":
+        order = get_object_or_404(Order, pk=order_id)
+        order_status = request.POST.get("order_status")
+        order.order_status = order_status
+        order.save()
+    return redirect(request.META.get('HTTP_REFERER', 'user_orders'))
