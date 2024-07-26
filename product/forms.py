def7b3e8a62b4a378520c3e7f58470d251c55eaa
Parneet <kaur311@uwindsor.ca>
2024-07-25 22:43:24 -0400

    Enhanced auction forms

--- product/forms.py
+++ product/forms.py
@@ -6,3 +6,9 @@ class AuctionForm(forms.ModelForm):
     class Meta:
         model = Auction
         fields = ['product', 'current_bid', 'start_time', 'end_time']
+        widgets = {
+            'product': forms.Select(attrs={'class': 'form-control'}),
+            'current_bid': forms.TextInput(attrs={'class': 'form-control'}),
+            'start_time': forms.TextInput(attrs={'class': 'form-control'}),
+            'end_time': forms.TextInput(attrs={'class': 'form-control'}),
+        }
